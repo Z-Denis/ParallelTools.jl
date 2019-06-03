c4f39e20a9e5e80e903fe4cebd0b3c6ee4447a04
Z-Denis <zakari.denis@univ-paris-diderot.fr>
2019-06-03 15:54:02 +0200

    Fix exports.

--- src/ParallelTools.jl
+++ src/ParallelTools.jl
@@ -2,6 +2,6 @@ module ParallelTools
 
 using Distributed
 include("concurrent_saver.jl")
-export ConcurrentSaver, launch_saver, @with_saver
+export ConcurrentSaver, launch_saver, @with_saver, @save_to
 
 end # module
